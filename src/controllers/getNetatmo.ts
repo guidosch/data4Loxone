48a7559d35eab92e13a5efe5ee45e8af4eda9340
guidosch <guido.schnider@gmail.com>
2023-10-19 21:12:26 +0200

    Handle incorrect http status code returned.

--- src/controllers/getNetatmo.ts
+++ src/controllers/getNetatmo.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express'
 import AxiosInterceptor from '../logic/netatmoRequestResponseInterceptor'
 import { MainStation } from '../logic/devices';
 import { ModuleDashboardData, Stationsdata } from '../types/stationsData';
-import { NetatmoStationsData, emtpyNetatmoStationsData } from '../types/netatmoStationsData4Loxone';
+import { NetatmoStationsData } from '../types/netatmoStationsData4Loxone';
 
 const axiosInterceptor = new AxiosInterceptor();
 const config = {
@@ -10,15 +10,18 @@ const config = {
 }
 
 let error: string = "";
+let cachedNetatmoStationsData: NetatmoStationsData;
 
 
 const getRoot: RequestHandler = async (req, res) => {
     let response = await axiosInterceptor.get('api/getstationsdata', config);
     
     if (response.status !== 200) {
-        error = response.status + " " + response.statusText
-        emtpyNetatmoStationsData.error = error;
-        res.status(response.status).json(emtpyNetatmoStationsData);
+        console.error("Netatmo API error: " + response.status + " " + response.statusText);
+        error = "Returning cached netatmo data because of error. Error: "+response.status + " " + response.statusText
+        //todo do try to return the last known data instead of empty data
+        cachedNetatmoStationsData.error = error;
+        res.status(503).json(cachedNetatmoStationsData);
     } else {
         const stationsdata: Stationsdata = response.data.body.devices[0];
 
@@ -35,8 +38,9 @@ const getRoot: RequestHandler = async (req, res) => {
             temperatureOutside: outdoorModule?.Temperature || 20,
             humidityOutside: outdoorModule?.Humidity || 50,
             lastUpdateSecondsAgo: Math.round(Date.now() / 1000) - (stationsdata.dashboard_data.time_utc),
-            error: error
+            error: "no error"
         }
+        cachedNetatmoStationsData = netatmoStationsData;
         res.status(200).json(netatmoStationsData);
     }
 
